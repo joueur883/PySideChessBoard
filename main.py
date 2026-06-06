bbe66146a18f3545532e5af512915a2c3c887db2
joueur883 <alexandre.heinrich20@gmail.com>
2026-06-06 14:15:53 +0200

    Update preview

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from widgets.chessboard import ChessBoard
 app = QApplication()
 
 board = ChessBoard()
-board.setFen("8/P7/8/8/7k/8/8/4K3 w - - 0 1")
+#board.setFen("8/P7/8/8/7k/8/8/4K3 w - - 0 1")
 board.show()
 
 app.exec()
